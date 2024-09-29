09354ff669e230a020334b6ec080fa9bd51ac80b
theheemmanuel <oladirane@gmail.com>
2024-09-29 13:01:01 +0100

    search bar

--- src/Components/Folder.jsx
+++ src/Components/Folder.jsx
@@ -1,21 +1,35 @@
 import { useContext } from "react";
 import { ContextFile } from "./FileContext";
+import { useState } from "react";
 
 const Folder = () => {
   const { allNotes, state, dispatch } = useContext(ContextFile);
+  const [search, setSearch] = useState("");
+  const currentFolder = allNotes.filter((each) => each.folder === state.folder);
+  const filteredNotes =
+    search === ""
+      ? allNotes
+      : allNotes.filter((note) =>
+          note.title.toLowerCase().includes(search.toLowerCase())
+        );
   return (
     <div className="w-1/2 min-h-[100vh] bg-[#1C1C1C] px-4 py-6 text-white">
       <div className="h-fit sticky top-6">
-        {/* <input
-        type="text"
-        placeholder="Search..."
-        className="border-2 border-gray-300 rounded-lg bg-transparent px-4 py-2 w-full outline-none"
-      /> */}
         {state.folder === "" ? (
           <div>
-            <h1 className="text-lg font-semibold">All Notes</h1>
+            <h1 className="text-lg font-semibold">All Notes</h1>{" "}
+            <input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              type="text"
+              placeholder="Search..."
+              className="border-[1px] border-gray-300 rounded-lg bg-transparent px-4 py-2 w-full outline-none"
+            />
             <div className="flex flex-col gap-2">
-              {allNotes.map((item) => (
+              {filteredNotes.length === 0 && (
+                <p className="text-center py-6">No Notes Found</p>
+              )}
+              {filteredNotes.map((item) => (
                 <div
                   onClick={() =>
                     dispatch({ type: "PickNote", payload: item.id })
@@ -37,24 +51,22 @@ const Folder = () => {
           <div>
             <h1 className="text-lg font-semibold">{state.folder}</h1>
             <div className="flex flex-col gap-2">
-              {allNotes
-                .filter((each) => each.folder === state.folder)
-                .map((item) => (
-                  <div
-                    key={item.id}
-                    className={`mt-2 p-3 cursor-pointer bg-[#FFFFFF08] hover:bg-[#FFFFFF1A] ${
-                      state.note === item.id && "bg-[#FFFFFF1A]"
-                    }`}
-                    onClick={() =>
-                      dispatch({ type: "PickNote", payload: item.id })
-                    }
-                  >
-                    <p className="font-semibold">{item.title}</p>
-                    <p className="text-sm pt-1">
-                      {new Date(item.date).toLocaleDateString()}
-                    </p>
-                  </div>
-                ))}
+              {currentFolder.map((item) => (
+                <div
+                  key={item.id}
+                  className={`mt-2 p-3 cursor-pointer bg-[#FFFFFF08] hover:bg-[#FFFFFF1A] ${
+                    state.note === item.id && "bg-[#FFFFFF1A]"
+                  }`}
+                  onClick={() =>
+                    dispatch({ type: "PickNote", payload: item.id })
+                  }
+                >
+                  <p className="font-semibold">{item.title}</p>
+                  <p className="text-sm pt-1">
+                    {new Date(item.date).toLocaleDateString()}
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
         )}
